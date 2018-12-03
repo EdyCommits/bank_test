7cfe4128e6b81a31f1f293dfec56626cfef1cac5
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-03 18:38:27 +0000

    Added test to check multiple transactions are updating balance properely.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -24,4 +24,13 @@ INITIAL_BALANCE = 0
     expect(account.current_balance).to eq(3)
   end
 
+  it 'deducts and adds money to current balance' do
+    account = Account.new
+    account.deposit(50)
+    account.withdraw(25)
+    account.deposit(1)
+
+    expect(account.current_balance).to eq(26)
+  end
+
 end
