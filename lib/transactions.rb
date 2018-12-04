631e9bfb150a6eb7deed3c0e1d3eef29ff8eb78b
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-04 14:25:17 +0000

    Built initialize transactions method with date,credit, debit

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,8 +1,10 @@
 class Transactions
-  
-  attr_reader :date
 
-  def initialize(date)
+  attr_reader :date, :credit, :debit
+
+  def initialize(date = Time.now.strftime('%d/%m/%Y'), deposit, withdraw)
     @date = date
+    @credit = deposit
+    @debit = withdraw
   end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,10 +2,18 @@ require 'transactions'
 describe Transactions do
 
   let(:date) { double :date }
-  subject { Transactions.new(date) }
+  subject { Transactions.new(date, 100, 10) }
 
   it 'stores date of a transaction' do
     expect(subject.date).to eq date
+  end
+
+  it 'stores deposited amount as credit' do
+    expect(subject.credit).to eq 100
+  end
+
+  it 'stores withdrawn amount as debit' do
+    expect(subject.debit).to eq 10
   end
 
 end
