9c518054e5a9d31de3cb80966029aaf513a3cf73
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-05 16:43:29 +0000

    Fix property name

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -4,7 +4,7 @@ class StatementPrinter
     puts 'Date || Credit || Debit || Balance'
 
     transactions.sort! { |x,y| y.date <=> x.date }.each do |transaction|
-      puts "#{transaction.date.strftime('%d/%m/%Y')} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"
+      puts "#{transaction.date.strftime('%d/%m/%Y')} || #{transaction.credit} || #{transaction.debit} || #{transaction.current_balance}"
     end
   end
 
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -6,8 +6,8 @@ describe StatementPrinter do
     past = Time.utc(2017,10,21)
     latest = Time.utc(2017,10,22)
 
-    latest_transaction = double(:latest_transaction, date: latest, credit: 100, debit: 0, balance: 100)
-    past_transaction = double(:old_transaction, date: past, credit: 0, debit: 20, balance: 80)
+    latest_transaction = double(:latest_transaction, date: latest, credit: 100, debit: 0, current_balance: 100)
+    past_transaction = double(:old_transaction, date: past, credit: 0, debit: 20, current_balance: 80)
 
     expect { subject.print([latest_transaction, past_transaction]) }.to output("Date || Credit || Debit || Balance\n22/10/2017 || 100 || 0 || 100\n21/10/2017 || 0 || 20 || 80\n").to_stdout
   end
@@ -16,8 +16,8 @@ describe StatementPrinter do
     past = Time.utc(2017,10,21)
     latest = Time.utc(2017,10,22)
 
-    latest_transaction = double(:latest_transaction, date: latest, credit: 100, debit: 0, balance: 100)
-    past_transaction = double(:old_transaction, date: past, credit: 0, debit: 20, balance: 80)
+    latest_transaction = double(:latest_transaction, date: latest, credit: 100, debit: 0, current_balance: 100)
+    past_transaction = double(:old_transaction, date: past, credit: 0, debit: 20, current_balance: 80)
 
     expect { subject.print([past_transaction, latest_transaction]) }.to output("Date || Credit || Debit || Balance\n22/10/2017 || 100 || 0 || 100\n21/10/2017 || 0 || 20 || 80\n").to_stdout
   end
