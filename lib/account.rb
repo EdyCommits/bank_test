07ebb55b3e1e15fb48e6c2dc8f88ef3f6251da80
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-05 09:59:59 +0000

    Added update log functionality when transaction is made.

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,13 @@
+require_relative 'transactions'
+require_relative 'transaction_log'
+
 class Account
 
   INITIAL_BALANCE = 0
 
-  def initialize
+  def initialize(transaction_log = TransactionLog.new)
     @balance = INITIAL_BALANCE
+    @transaction_log = transaction_log
   end
 
   def current_balance
@@ -12,6 +16,7 @@ class Account
 
   def deposit(amount)
     @balance += amount
+    @transaction_log.add([amount, 0, @balance])
   end
 
   def withdraw(amount)
